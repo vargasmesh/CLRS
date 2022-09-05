cbc1c323533e310cc9192d50acc6d68ad35dc141
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2022-09-05 10:02:52 -0300

    Chore(clojure): Add Substitution Model to Insertion Sort

--- chapter-2/insertion-sort/clojure/project.clj
+++ chapter-2/insertion-sort/clojure/project.clj
@@ -1,5 +1,6 @@
 (defproject insertion-sort "0.1.0-SNAPSHOT"
   :repl-options {:init-ns insertion-sort}
   :eval-in-leiningen true
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]}})
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]
+                             [org.clojure/tools.trace "0.7.11"]]}})
 
