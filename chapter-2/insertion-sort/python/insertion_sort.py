a0ce246dde3bb691e5063e4ec7dd3dcf472ee19d
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2022-09-18 12:16:25 -0300

    Refactor(python): Improve insertion sort readability

--- chapter-2/insertion-sort/python/insertion_sort.py
+++ chapter-2/insertion-sort/python/insertion_sort.py
@@ -1,10 +1,7 @@
 def insertion_sort(array):
     for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
+        j = i
 
-        while j >= 0 and array[j] > key:
-            array[j+1] = array[j]
+        while j > 0 and array[j - 1] > array[j]:
+            array[j - 1], array[j] = array[j], array[j - 1]
             j -= 1
-
-        array[j+1] = key
