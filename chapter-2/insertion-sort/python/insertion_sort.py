879e7c5645b8d6a2436d10d2a1ab340de5769906
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2022-10-16 11:09:55 -0300

    Revert: Improve insertion sort readability
    This reverts commit a0ce246dde3bb691e5063e4ec7dd3dcf472ee19d.

--- chapter-2/insertion-sort/python/insertion_sort.py
+++ chapter-2/insertion-sort/python/insertion_sort.py
@@ -1,7 +1,10 @@
 def insertion_sort(array):
     for i in range(1, len(array)):
-        j = i
+        key = array[i]
+        j = i - 1
 
-        while j > 0 and array[j - 1] > array[j]:
-            array[j - 1], array[j] = array[j], array[j - 1]
+        while j >= 0 and array[j] > key:
+            array[j+1] = array[j]
             j -= 1
+
+        array[j+1] = key
